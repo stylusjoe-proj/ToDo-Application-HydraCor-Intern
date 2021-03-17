3f9821e8123b75d17e96ee8e90c8d90e14ed9fe0
dom-hydracor-intern <domonique.walcott@hydracor.net>
2021-03-17 09:25:07 -0400

    update to handle user input in local storage

--- js/main.js
+++ js/main.js
@@ -17,20 +17,30 @@ let app = new Vue({
                 text: 'Introduce Persistent Storage',
                 completed: false
             }
+
         ],
         welcomeMessage: 'The Introductory Application',
-        inputText:'blank'
+        inputText: ''
     },
     methods: {
         markComplete() {
-            this.todo.completed = !this.todo.completed;
+            this.todos.completed = !this.todos.completed;
+            console.log(test123)
+        },
+        getItem() {
+            let thisItem = localStorage.getItem('item');
+            console.log(thisItem)
         }
     },
     mounted() {
-
+        this.$nextTick(function () {
+            /* addItem() */
+        });
     },
 });
 
+
+/*
 function deleteItemFunction() {
     console.log(123)
 }
@@ -38,3 +48,14 @@ function deleteItemFunction() {
 function addItemFunction() {
     console.log(321)
 }
+
+function addItem() {
+    var input = message;
+    localStorage.setItem('item', input);
+}
+
+function getItem() {
+    let thisItem = localStorage.getItem('item');
+    console.log(thisItem)
+}
+*/
